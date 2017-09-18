319100f2719787296b58a8252a869301add00c8b
Victor Guerra Veloso <victorgvbh@gmail.com>
2017-09-18 00:38:17 -0300

    Toques finais, cmake modificado para c e nao c++, gerador de entrada aleatoria agora recebe argumento que define tamanho de entrada

--- geraraleatorio.py
+++ geraraleatorio.py
@@ -1,9 +1,15 @@
 from random import random
-print (10000000)
-copia1 = int(random()*50000000)
-copia2 = int(random()*50000000)
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("CoeficienteDoTamanhoDeEntrada", help="Coeficiente do Tamanho de Entrada")
+coeficiente = int(parser.parse_args().CoeficienteDoTamanhoDeEntrada)
+
+print (100000*coeficiente)
+copia1 = int(random()*500000*coeficiente)
+copia2 = int(random()*500000*coeficiente)
 print (copia1+copia2)
 print (copia1)
 print (copia2)
-for i in range(2,1000000):
-    print(int(random()*50000000))
+for i in range(2,10000*coeficiente):
+    print(int(random()*500000*coeficiente))
